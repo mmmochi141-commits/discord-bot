c6037dc6fbabddd57a0cc922bf42ccd12ca38203
mmmochi141-commits <mmmochi141@gmail.com>
2026-07-16 17:50:10 +0900

    Create post.py

---
+++ post.py
@@ -0,0 +1,35 @@
+import csv
+import io
+import os
+import random
+import urllib.request
+import json
+
+CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vT3eRvVcQUfCRU42X_jiZa2-l84LvrA2zLn_dblRsgQ5w-9OLxw2PQ_kOSWAsMmi1fJ9-RIS84W5t_T/pub?gid=0&single=true&output=csv"
+DISCORD_TOKEN = os.environ["DISCORD_TOKEN"]
+CHANNEL_ID = os.environ["CHANNEL_ID"]
+
+def get_programs():
+    with urllib.request.urlopen(CSV_URL) as res:
+        content = res.read().decode("utf-8")
+    reader = csv.DictReader(io.StringIO(content))
+    return list(reader)
+
+programs = get_programs()
+pick = random.choice(programs)
+
+data = {
+    "content": f"📺 **今週のおすすめ番組！**\n\n🎬 **{pick['タイトル']}**\n🔗 {pick['URL']}"
+}
+
+url = f"https://discord.com/api/v10/channels/{CHANNEL_ID}/messages"
+req = urllib.request.Request(
+    url,
+    data=json.dumps(data).encode("utf-8"),
+    headers={
+        "Authorization": f"Bot {DISCORD_TOKEN}",
+        "Content-Type": "application/json",
+    },
+    method="POST",
+)
+urllib.request.urlopen(req)
